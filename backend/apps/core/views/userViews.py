da8fb564007aa3b4f309ab054e0b45f02fb91936
calango <mozartnp@gmail.com>
2022-10-22 14:27:13 -0300

    Create app pessoas

--- backend/apps/core/views/userViews.py
+++ backend/apps/core/views/userViews.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 from backend.apps.custom_user.models import CustomUser
 from backend.apps.custom_user.forms import CreateUserForms
@@ -23,3 +24,16 @@ class CreateUserCreateView(CreateView):
     model = CustomUser
     template_name = 'user/create_user.html'
     form_class = CreateUserForms
+
+    def get_success_url(self):
+        '''
+        Define o redirect dependendo do tipo de usuário.
+        '''
+        if self.object.is_client:
+            print('É cliente')
+            # return reverse_lazy('patient:doctor_list') #TODO
+        elif not self.object.is_client:
+            print('É empresa')
+            # return reverse_lazy('patient:patient_list') #TODO
+
+        return reverse_lazy('core:login_custom')
